b1d954ef10c8088cd69bf079edbf9857e878d049
isaacvautsai <isaacv.dev.autsai@gmail.com>
2025-02-03 02:57:12 +0100

    6 Commit

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 from veritas_fact_check_api import app
+import os
 
 # This is the application object needed by gunicorn
 application = app
 
+# Configure the application
+application.config['PROPAGATE_EXCEPTIONS'] = True
+application.config['DEBUG'] = False
+
 if __name__ == "__main__":
-    application.run() 
+    port = int(os.environ.get('PORT', 3000))
+    application.run(host='0.0.0.0', port=port) 
